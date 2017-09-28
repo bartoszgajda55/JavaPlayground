edcc6989aca8df5b76c1b03913ffaea9377661e1
Bartosz Gajda <bg@bartoszgajda.com>
2017-09-28 20:07:16 +0100

    Encapsulating a class,

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+public class Car {
+    // State of car (fields)
+    private int doors;
+    private int wheels;
+    private String maker;
+    private String engine;
+    private String colour;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
