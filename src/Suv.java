f6bb1f6e3562e5eef83944ed7aa84f082cb0f597
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-02 19:30:09 +0100

    whatever.

--- src/Suv.java
+++ src/Suv.java
@@ -1,5 +1,8 @@
 public class Suv extends Car {
+    private String drivetrain;
+
     public Suv(int doors, int wheels, String maker, String engine, String colour) {
         super(doors, wheels, maker, engine, colour);
+        this.drivetrain = "AWD";
     }
 }
