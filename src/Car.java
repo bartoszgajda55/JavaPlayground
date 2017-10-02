1c68c5292d11e1184b04ae25c0c1b8b8482eb3c6
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-02 19:16:28 +0100

    ...

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,23 @@
 public class Car {
     // State of car (fields)
+
     private int doors;
     private int wheels;
     private String maker;
     private String engine;
     private String colour;
+
+    public Car(int doors, int wheels, String maker, String engine, String colour) {
+        this.doors = doors;
+        this.wheels = wheels;
+        this.maker = maker;
+        this.engine = engine;
+        this.colour = colour;
+    }
+    public Car() {
+        // Using constructor by passing default values.
+        this(4, 4, "Ford", "V8", "Red");
+    }
 
     public int getDoors() {
         return doors;
