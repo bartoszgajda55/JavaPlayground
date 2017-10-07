be66d4e3c7028da039e2b25290f4eb695e43981f
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-07 20:12:07 +0100

    Lambdas intro.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        new Thread(() -> System.out.println("From runnable")).start();
+
+        Employee john = new Employee("dasdasd", 23);
+        Employee john2 = new Employee("dxczcxzx", 33);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(john2);
+
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        for (Employee employee : employees) {
+            System.out.println(employee.getName());
+        }
+    }
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
