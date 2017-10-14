4e152c7d0ef683f6c25fbbaafbab44f18aa74e09
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-14 16:59:28 +0100

    Defined base classes for polymorphism.

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,64 @@ public class Main {
 
     }
 }
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot";
+    }
+}
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over universe";
+    }
+}
+
+class ForgetableMovie extends Movie {
+    public ForgetableMovie() {
+        super("Forgetable movie");
+    }
+}
